4db4b10d6ea2c009dadff51e3f937101ee85d5d1
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2026-07-17 21:52:50 +0200

    devme v0.5.1

--- Formula/devme.rb
+++ Formula/devme.rb
@@ -2,22 +2,22 @@ class Devme < Formula
   desc "The executable README — one command from git clone to running dev environment"
   homepage "https://devme.sh"
   license "MIT"
-  version "0.5.0"
+  version "0.5.1"
 
   on_macos do
     url "https://github.com/devme-sh/devme/releases/download/v#{version}/devme-aarch64-apple-darwin.tar.gz"
-    sha256 "443e86e9cd1244a7af654f734fab48b02c36cec0c6942b9cfca49c7568101cd5"
+    sha256 "8ceb252748f8b76bcf6cf666be8b4bf51ab668a7711b3f223a89ea10841db143"
   end
 
   on_linux do
     on_arm do
       url "https://github.com/devme-sh/devme/releases/download/v#{version}/devme-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ac9f24882dcf90b5f225c5f221fad300e5f8eeb0ded1969535776535756ee428"
+      sha256 "1ccef4526accb87a4d3b508159566019e4597fa0a1baae8e512062dd868c07ac"
     end
 
     on_intel do
       url "https://github.com/devme-sh/devme/releases/download/v#{version}/devme-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8d1f7ca6c240af5891b72630f380460d61e3f7206e6162b8b5fa54f77fe872cf"
+      sha256 "d56edd9ec9d6ef1a6ce3a399ea2b181c3a52aa8b77e8dd3e7b71b66f3e135a8a"
     end
   end
 
